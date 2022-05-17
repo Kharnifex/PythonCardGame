1ca58ae193dbb68dcb60dcd71398271319bdc855
Kharnifex <61503559+Kharnifex@users.noreply.github.com>
2022-05-18 02:22:08 +0300

    testing pygame stuff

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("testtest123")
+import pygame
+pygame.init()
+WIDTH, HEIGHT = 900, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("testing this atm")
+
+black = 0, 0, 0
+
+card = pygame.image.load("assets//cardlol.png")
+cardrect = card.get_rect()
+
+def main():
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        WIN.fill(black)
+        WIN.blit(card, cardrect)
+        pygame.display.flip()
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
